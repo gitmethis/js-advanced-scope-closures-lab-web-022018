0200e86f38e0bafe9f16fa08c97d73639d786338
Chase <chaseit12@gmail.com>
2018-04-02 19:03:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function produceDrivingRange(range){
   return function(start_range, end_range){
-    if (Math.abs(start_range-end) < 10){
+    if (Math.abs(start_range-end_range) < 10){
         return true
-    }else if (Math.abs(start_range-end) < 10){
+    }else if (Math.abs(start_range-end_range) < 10){
         return false
     }
   }
