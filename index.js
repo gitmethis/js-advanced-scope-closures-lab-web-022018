8d21882d39166e23e2025e1b94345a3f3c947d91
Chase <chaseit12@gmail.com>
2018-04-02 19:28:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function produceDrivingRange(range){
       start_range = parseInt(start_range)
   end_range = parseInt(end_range)
     if (Math.abs(start_range-end_range) < 10){
-        return `within range by ${range - Math.abs(start_range-end_range)}`
+        return `within range by ${Math.abs(range - Math.abs(start_range-end_range))}`
     }else {
         return `${range-Math.abs(start_range-end_range)} blocks out of range.`
     }
